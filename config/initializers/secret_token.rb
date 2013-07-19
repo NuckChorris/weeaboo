e7030e7b0f921590f7e22c7bee2772665dc99991
Peter Lejeck <peter.lejeck@gmail.com>
2013-07-18 19:11:48 -0700

    Rails?  More like YAY-ELS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weeaboo::Application.config.secret_key_base = 'f551e7de99a4d7fdf978227e5de19b6e140b0841c79f7d9e5c15e390344b964e80f24983d3d70ed3ad00c73fc9d34abd3d614cb2deade45cc6fc3d2e89632c0d'
